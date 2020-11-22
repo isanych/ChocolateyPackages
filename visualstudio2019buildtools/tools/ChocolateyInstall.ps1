5b22970df9c4319b5631dbe24daf4e1d1bb08e59
Igor Kostenko <isanych@gmail.com>
2020-11-22 15:48:40 +0000

    Use version 16.7.8 of buildtools installer

--- visualstudio2019buildtools/tools/ChocolateyInstall.ps1
+++ visualstudio2019buildtools/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2019buildtools' `
     -ApplicationName 'Microsoft Visual Studio Build Tools 2019' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/9d2147aa-7b01-4336-b665-8fe07735e5ee/c4b2212532e637ae35783660e79211e0604d0b61e54f6c7db69df30ce446bd73/vs_BuildTools.exe' <# https://aka.ms/vs/16/release/vs_buildtools.exe #> `
-    -Checksum 'C4B2212532E637AE35783660E79211E0604D0B61E54F6C7DB69DF30CE446BD73' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/1206a800-42a6-4dd5-8b7d-27ccca92e823/cf739d701898f888a4c0b49722791e5ff450d40c6a986f69ecfb1e4da384e126/vs_BuildTools.exe' `
+    -Checksum 'CF739D701898F888A4C0B49722791E5FF450D40C6A986F69ECFB1E4DA384E126' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'BuildTools' `
