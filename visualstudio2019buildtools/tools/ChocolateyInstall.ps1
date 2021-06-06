f58d354a689991ed3cd0b875524a581358b9be90
Igor Kostenko <ikostenko@perforce.com>
2021-06-06 10:06:41 +0100

    version 16.7.15

--- visualstudio2019buildtools/tools/ChocolateyInstall.ps1
+++ visualstudio2019buildtools/tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿Install-VisualStudio `
     -PackageName 'visualstudio2019buildtools' `
     -ApplicationName 'Microsoft Visual Studio Build Tools 2019' `
-    -Url 'https://download.visualstudio.microsoft.com/download/pr/fd0b4484-1a13-48cb-a0c3-398aa45a4382/44f6322c54305aab47b071ecd3b9efd599f41a4c153d2084cdff52c19c8b9479/vs_BuildTools.exe' `
-    -Checksum '44F6322C54305AAB47B071ECD3B9EFD599F41A4C153D2084CDFF52C19C8B9479' `
+    -Url 'https://download.visualstudio.microsoft.com/download/pr/027ff210-d6f8-43f4-be63-3316e2aa6ce2/70b89caa3ed83c95c4dde0c7dfbd1e65b4d30815e2a4f9fbba11aa1debb4e680/vs_BuildTools.exe' `
+    -Checksum '470B89CAA3ED83C95C4DDE0C7DFBD1E65B4D30815E2A4F9FBBA11AA1DEBB4E680' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -Product 'BuildTools' `
